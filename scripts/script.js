9f9df0438e7656bbee9e037697c95564c180a210
unknown <melya89bk@gmail.com>
2021-04-10 16:15:35 +0300

    initial

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".burger__active").addEventListener('click',(e)=>{ e.preventDefault()
+  // console.log('clicked')
+document. querySelector(".media__header-menu").classList.toggle('burger')
+document.querySelector(".burger__active").classList.toggle('burger__btn')
+})
